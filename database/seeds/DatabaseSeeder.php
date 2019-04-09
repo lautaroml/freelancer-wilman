421d645ece2ca61ffc9ac5dd4a8f79fee4840c2a
Lautaro López <lautaroml@hotmail.com>
2019-04-09 16:33:48 -0500

    Added comunas seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(DepartamentosTableSeeder::class);
+        $this->call(MunicipiosTableSeeder::class);
+        $this->call(ComunasTableSeeder::class);
         $this->call(PerfilsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
     }
