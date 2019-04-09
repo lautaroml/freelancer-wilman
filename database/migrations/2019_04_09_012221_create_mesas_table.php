6e70e1ecb53e482ed45219e9c88987c15e5bb0bb
Lautaro López <lautaroml@hotmail.com>
2019-04-08 20:24:32 -0500

    Added mesas migration

---
+++ database/migrations/2019_04_09_012221_create_mesas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMesasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mesas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('nombre');
+            $table->unsignedInteger('comuna_id');
+            $table->unsignedInteger('barrio_id');
+            $table->unsignedInteger('puesto_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mesas');
+    }
+}
