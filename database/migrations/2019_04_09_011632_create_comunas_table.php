213e953a286fc1deff74ff52e219f5c8304cb611
Lautaro López <lautaroml@hotmail.com>
2019-04-08 20:18:23 -0500

    Added comunas migration

---
+++ database/migrations/2019_04_09_011632_create_comunas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComunasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comunas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('comuna_lat');
+            $table->string('comuna_long');
+            $table->unsignedInteger('departamento_id');
+            $table->unsignedInteger('municipio_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comunas');
+    }
+}
